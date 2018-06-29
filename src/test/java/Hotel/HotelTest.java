edab59bbcac1b6c1cb0f53729c80d20479bcdd6d
Gary Muir <garymuir17@gmail.com>
2018-06-29 15:00:37 +0100

    adds some more setup to HotelTest

--- src/test/java/Hotel/HotelTest.java
+++ src/test/java/Hotel/HotelTest.java
@@ -1,10 +1,11 @@
 package Hotel;
 
 import Room.RoomType.BedRoomType;
+import Room.RoomType.ConferenceRoom;
 import Room.RoomType.DiningRoom;
+import Guest.Guest;
 import org.junit.Before;
 import org.junit.Test;
-import Room.Room;
 import Room.RoomType.Bedroom;
 
 import static org.junit.Assert.assertEquals;
@@ -12,14 +13,36 @@ import static org.junit.Assert.assertEquals;
 public class HotelTest {
 
     Hotel hotel;
-    Bedroom bedroom;
+    Hotel hotelWithRooms;
+    Bedroom bedroom1;
+    Bedroom bedroom2;
     DiningRoom diningRoom;
+    ConferenceRoom conferenceRoom;
+    Guest guest1;
+    Guest guest2;
 
     @Before
     public void setUp() {
         hotel = new Hotel("California", 0);
-        bedroom = new Bedroom("17", BedRoomType.SINGLE, 20);
+
+        hotelWithRooms = new Hotel("Testing Hotel", 0);
+
+        bedroom1 = new Bedroom("17", BedRoomType.SINGLE, 20);
+
+        bedroom2 = new Bedroom("30", BedRoomType.DOUBLE, 50);
+
         diningRoom = new DiningRoom("The Grill", 2);
+
+        conferenceRoom = new ConferenceRoom("Room1", 1, 20);
+
+        guest1 = new Guest("Jack", 100);
+        guest2 = new Guest("Norman", 50);
+
+        hotelWithRooms.addRoom(bedroom1);
+        hotelWithRooms.addRoom(bedroom2);
+        hotelWithRooms.addRoom(diningRoom);
+        hotelWithRooms.addRoom(conferenceRoom);
+
     }
 
     @Test
@@ -39,7 +62,7 @@ public class HotelTest {
 
     @Test
     public void canAddBedRoom() {
-        hotel.addRoom(bedroom);
+        hotel.addRoom(bedroom1);
         assertEquals(1, hotel.numberOfRooms());
     }
 
@@ -51,9 +74,14 @@ public class HotelTest {
 
     @Test
     public void canAddTwoRoomsOfDifferentClasses() {
-        hotel.addRoom(bedroom);
+        hotel.addRoom(bedroom1);
         hotel.addRoom(diningRoom);
         assertEquals(2, hotel.numberOfRooms());
+    }
+
+    @Test
+    public void checkWhetherSetUpHotelHasRooms() {
+        assertEquals(4, hotelWithRooms.numberOfRooms());
     }
 
 
