b0f69cc28e33b180318f8313bfc5b189d3d7d934
Gary Muir <garymuir17@gmail.com>
2018-06-29 12:32:36 +0100

    sets up the ConferenceRoom class and uses all the tests that DiningRoom had

--- src/main/java/Room/RoomType/ConferenceRoom.java
+++ src/main/java/Room/RoomType/ConferenceRoom.java
@@ -1,15 +1,32 @@
 package Room.RoomType;
 
 import Room.Room;
+import Guest.Guest;
 
 public class ConferenceRoom extends Room {
 
-    public ConferenceRoom() {
+    private String name;
+    private int capacity;
+
+    public ConferenceRoom(String name, int capacity) {
         super();
+        this.name = name;
+        this.capacity = capacity;
     }
 
+    public String getName() {
+        return this.name;
+    }
 
+    public int getCapacity() {
+        return this.capacity;
+    }
 
-
+    @Override
+    public void addGuest(Guest guest) {
+        if (this.numberOfOccupants() < capacity) {
+            this.getOccupants().add(guest);
+        }
+    }
 
 }
--- src/test/java/Room/RoomType/ConferenceRoomTest.java
+++ src/test/java/Room/RoomType/ConferenceRoomTest.java
@@ -1,4 +1,80 @@
 package Room.RoomType;
 
+import org.junit.Before;
+import Guest.Guest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+
+    ConferenceRoom conferenceRoom;
+    Guest guest1;
+    Guest guest2;
+
+    @Before
+    public void setUp() {
+        conferenceRoom = new ConferenceRoom("Room1", 1);
+        guest1 = new Guest("Jack", 100);
+        guest2 = new Guest("Norman", 50);
+    }
+
+    @Test
+    public void occupantsArrayListStartsEmpty() {
+        assertEquals(0, conferenceRoom.numberOfOccupants());
+    }
+
+    @Test
+    public void canAddGuestToConferenceRoom() {
+        conferenceRoom.addGuest(guest1);
+        assertEquals(1, conferenceRoom.numberOfOccupants());
+    }
+
+    @Test
+    public void canRemoveGuestRoomIsOccupied() {
+        conferenceRoom.addGuest(guest1);
+        conferenceRoom.removeGuest();
+        assertEquals(0, conferenceRoom.numberOfOccupants());
+    }
+
+    @Test
+    public void cannotRemoveGuestWhenRoomIsAlreadyEmpty() {
+        conferenceRoom.removeGuest();
+        assertEquals(0, conferenceRoom.numberOfOccupants());
+    }
+
+    @Test
+    public void canEmptyRoom() {
+        conferenceRoom.addGuest(guest1);
+        conferenceRoom.addGuest(guest2);
+        conferenceRoom.emptyRoom();
+        assertEquals(0, conferenceRoom.numberOfOccupants());
+    }
+
+    @Test
+    public void cannotEmptyRoomAlreadyEmpty() {
+        conferenceRoom.emptyRoom();
+        assertEquals(0, conferenceRoom.numberOfOccupants());
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Room1", conferenceRoom.getName());
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(1, conferenceRoom.getCapacity());
+    }
+
+    @Test
+    public void cannotAddGuestWhenRoomIsFull() {
+        conferenceRoom.addGuest(guest1);
+        conferenceRoom.addGuest(guest2);
+        assertEquals(1, conferenceRoom.numberOfOccupants());
+    }
+
+
+
+
 }
