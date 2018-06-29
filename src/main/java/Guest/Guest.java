9612631c016a35a75e9f2f9590b43f98ec692478
Gary Muir <garymuir17@gmail.com>
2018-06-29 11:26:34 +0100

    sets up the Guest class, adds a getter for name

--- src/main/java/Guest/Guest.java
+++ src/main/java/Guest/Guest.java
@@ -1,4 +1,17 @@
 package Guest;
 
 public class Guest {
+
+    private String name;
+    private int wallet;
+
+    public Guest(String name, int wallet) {
+        this.name = name;
+        this.wallet = wallet;
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
 }
--- src/test/java/Guest/GuestTest.java
+++ src/test/java/Guest/GuestTest.java
@@ -1,4 +1,22 @@
 package Guest;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+    Guest guest;
+
+    @Before
+    public void setUp() {
+        guest = new Guest("Jack", 100);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Jack", guest.getName());
+    }
+
 }
